eedcfc4ce3930a7975d477f6957a3d6611d6b22e
lh <1178285946@qq.com>
2021-04-27 22:43:50 +0800

    新增上传

--- mybatis-05/src/main/java/com/kuang/dao/UserMapper.java
+++ mybatis-05/src/main/java/com/kuang/dao/UserMapper.java
@@ -2,11 +2,20 @@ package com.kuang.dao;
 
 
 import com.kuang.pojo.User;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 public interface UserMapper {
-    @Select("select * for user")
-    List<User> getUserAll();
+    @Select("select * from user")
+    List<User> getUsers();
+    
+    @Insert("insert into user (id,name,pwd) values (#{id},#{name},#{password})")
+    int addUser(User user);
+
+    @Delete("delete from user where id=#{uid}")
+    int deUser(@Param("uid") int id);
+
+    @Update("update user set name=#{name},pwd=#{password} where id=#{id}")
+    int updateUsers(User user);
 }
